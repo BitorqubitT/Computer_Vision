011054cf11c16eaa961926003aa82fe74414480f
BitorqubitT <thierryars2@hotmail.com>
2020-01-20 19:21:18 +0100

    added sobel-x filter, needs a lot of improvement

--- Sobel.py
+++ Sobel.py
@@ -1,10 +1,8 @@
+# TODO: Add y-sobel filter, combine them, get rid of ugly code, Add function for other filters.
 # Sobel filter in python.
 # Load image as matrix
 # Convert to gray scale
 # Perform sobel-operator
-# Output
-
-# Image uses PIL/ PILLOW
 import cv2
 from PIL import Image
 import numpy as np
@@ -16,37 +14,43 @@ def getImage(image_path):
     image = cv2.imread(image_path)
     # Convert from bgr to rgb
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    print(img, "lalalaal")
     return(img)
-
-
 
 # Convert image to Grayscale
 # Y = 0.299 R0 + 0.587 G + 0.114 B
-
 def rgbtoGray(matrix):
     new = np.ndarray(shape=(matrix.shape[0], matrix.shape[1], 1), dtype=int, order='F')
 
     for i in range(0, matrix.shape[0]):
         luminant = 0
         for j in range(0, matrix.shape[1]):
-            for k in range(0, matrix.shape[2]):
-                if k is 0:
-                    luminant = luminant + matrix[i,j,k] * 0.299
-                elif k is 1:
-                    luminant = luminant + matrix[i,j,k] * 0.587
-                elif k is 2:
-                    luminant = luminant + matrix[i,j,k] * 0.114
-                    new[i,j] = luminant
-                    luminant = 0
-    print(new)
+            luminant = matrix[i,j,0] * 0.299 + luminant + matrix[i,j,1] * 0.587 + luminant + matrix[i,j,2] * 0.114
+            new[i,j] = luminant
+            luminant = 0
     return new
 
 # Sobel Filter
-def sobel(image):
+def sobel(matrix):
     # Define sobel kernel
     Gx = np.array([[-1, 0, 1], [-1, 0, 1,], [-1, 0, 1]])
     Gy = np.array([[-1, -1, -1], [0, 0, 0,], [1, 1, 1]])
+
+    new = np.ndarray(shape=(matrix.shape[0], matrix.shape[1], 1), dtype=int, order='F')
+    # BLEHHHHHHHHHHH very ugly
+    for i in range(0, matrix.shape[0] - 1):
+        for j in range(0, matrix.shape[1] - 1):
+            value1 = matrix[i-1, j-1] * 1
+            value2 = matrix[i-1, j] * 0
+            value3 = matrix[i-1, j+1] * -1
+            value4 = matrix[i, j-1] * 1
+            value5 = matrix[i, j] * 0
+            value6 = matrix[i, j+1] * -1
+            value7 = matrix[i+1, j-1] * 1
+            value8 = matrix[i+1, j] * 0
+            value9 = matrix[i+1, j+1] * -1
+            value = value1 + value2 + value3 + value4 + value5 + value6 + value7 + value8 + value9
+            new[i, j] = value
+    return new
 
 # Create image from array
 def createImage(image):
@@ -54,9 +58,12 @@ def createImage(image):
     # Maybe matplotlib better?
     # Might want to use a different mode.
     image = Image.fromarray(image, mode = 'CMYK')
-
     image.show()
 
-x = rgbtoGray(getImage(image_path))
+def main():
+    x = rgbtoGray(getImage(image_path))
+    done = sobel(x)
+    createImage(done)
 
-createImage(x)
+if __name__ == "__main__":
+    main()
